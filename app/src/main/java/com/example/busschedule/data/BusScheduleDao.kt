d298e6812301e41ab5f155ad3f17c35a0e65d925
EduardoLaraAyala <61323192+EduardoLaraAyala@users.noreply.github.com>
2023-05-30 10:57:43 -0600

    agregue algunos comentarios

--- app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -7,12 +7,14 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BusScheduleDao {
+   /*me da ell tiempo de llegada en orden ascendente*/
     @Query(
         """
         SELECT * FROM schedule 
         ORDER BY arrival_time ASC    
         """
     )
+   /*obtener todos los registros de la tabla schedule*/
     fun getAll(): Flow<List<BusSchedule>>
 
     @Query(
@@ -22,5 +24,10 @@ interface BusScheduleDao {
         ORDER BY arrival_time ASC 
         """
     )
+
+    /*realiza una consulta a la base de datos para obtener los registros
+    de la tabla schedule que coinciden con el nombre de parada stopName.
+    y los ordena ascendentemente dependiendo su hora*/
+
     fun getByStopName(stopName: String): Flow<List<BusSchedule>>
 }
